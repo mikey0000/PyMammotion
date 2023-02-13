83e749e3f70ebcc70449c5814aec280dd09ac3c2
Michael Arthur <michael.arthur@propellerhead.co.nz>
2023-02-14 11:58:29 +1300

    update proto on pc with protoc

--- proto/esp_driver_pb2.py
+++ proto/esp_driver_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10\x65sp_driver.proto\"\xaf\x01\n\x07\x43ommEsp\x1a+\n\x0f\x44rvDevInfoReqId\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04type\x18\x02 \x01(\x05\x1a:\n\rDrvDevInfoReq\x12)\n\x07req_ids\x18\x01 \x03(\x0b\x32\x18.CommEsp.DrvDevInfoReqId\x1a;\n\x0e\x44rvDevInfoResp\x12)\n\x07req_ids\x18\x01 \x03(\x0b\x32\x18.CommEsp.DrvDevInfoReqIdb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10\x65sp_driver.proto\"\xaa\x02\n\x07\x43ommEsp\x12\x33\n\x11todev_devinfo_req\x18\x0b \x01(\x0b\x32\x16.CommEsp.DrvDevInfoReqH\x00\x12\x35\n\x12toapp_devinfo_resp\x18\x0c \x01(\x0b\x32\x17.CommEsp.DrvDevInfoRespH\x00\x1a+\n\x0f\x44rvDevInfoReqId\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04type\x18\x02 \x01(\x05\x1a:\n\rDrvDevInfoReq\x12)\n\x07req_ids\x18\x01 \x03(\x0b\x32\x18.CommEsp.DrvDevInfoReqId\x1a<\n\x0e\x44rvDevInfoResp\x12*\n\x08resp_ids\x18\x01 \x03(\x0b\x32\x18.CommEsp.DrvDevInfoReqIdB\x0c\n\nEspSubTypeb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'esp_driver_pb2', globals())
@@ -21,11 +21,11 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _COMMESP._serialized_start=21
-  _COMMESP._serialized_end=196
-  _COMMESP_DRVDEVINFOREQID._serialized_start=32
-  _COMMESP_DRVDEVINFOREQID._serialized_end=75
-  _COMMESP_DRVDEVINFOREQ._serialized_start=77
-  _COMMESP_DRVDEVINFOREQ._serialized_end=135
-  _COMMESP_DRVDEVINFORESP._serialized_start=137
-  _COMMESP_DRVDEVINFORESP._serialized_end=196
+  _COMMESP._serialized_end=319
+  _COMMESP_DRVDEVINFOREQID._serialized_start=140
+  _COMMESP_DRVDEVINFOREQID._serialized_end=183
+  _COMMESP_DRVDEVINFOREQ._serialized_start=185
+  _COMMESP_DRVDEVINFOREQ._serialized_end=243
+  _COMMESP_DRVDEVINFORESP._serialized_start=245
+  _COMMESP_DRVDEVINFORESP._serialized_end=305
 # @@protoc_insertion_point(module_scope)
