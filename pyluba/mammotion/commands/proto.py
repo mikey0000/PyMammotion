da0c1a3e98c89b48721280de713472047597a59d
Michael Arthur <michael.arthur@gameglass.gg>
2024-05-30 17:41:02 +1200

    add resume task function

--- pyluba/mammotion/commands/proto.py
+++ pyluba/mammotion/commands/proto.py
@@ -58,6 +58,27 @@ class LubaCommandProtoMQTT:
 
         return luba_msg.SerializeToString()
 
+
+    def resume_execute_task(self):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_NAV,
+            sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
+            rcver=luba_msg_pb2.MsgDevice.DEV_MAINCTL,
+            msgattr=luba_msg_pb2.MsgAttr.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            nav=mctrl_nav_pb2.MctlNav(
+                todev_taskctrl=mctrl_nav_pb2.NavTaskCtrl(
+                    type=1,
+                    action=3,
+                    result=0
+                )
+            )
+        )
+
+        return luba_msg.SerializeToString()
+
     async def return_to_dock(self):
         mctrlNav = mctrl_nav_pb2.MctlNav()
         navTaskCtrl = mctrl_nav_pb2.NavTaskCtrl()
