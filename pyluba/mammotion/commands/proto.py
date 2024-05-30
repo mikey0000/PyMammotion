29b89aab58ae3e73226aa5154470e6ada12d4909
Michael Arthur <michael.arthur@gameglass.gg>
2024-05-30 15:16:58 +1200

    fix missing import

--- pyluba/mammotion/commands/proto.py
+++ pyluba/mammotion/commands/proto.py
@@ -1,4 +1,4 @@
-from pyluba.proto import luba_msg_pb2, dev_net_pb2, mctrl_sys_pb2
+from pyluba.proto import luba_msg_pb2, dev_net_pb2, mctrl_sys_pb2, mctrl_nav_pb2
 from pyluba.proto.mctrl_sys import RptInfoType
 
 
@@ -17,6 +17,65 @@ class LubaCommandProtoMQTT:
             net=build)
 
         return luba_msg.SerializeToString()
+
+
+    async def start_work_job(self):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_NAV,
+            sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
+            rcver=luba_msg_pb2.MsgDevice.DEV_MAINCTL,
+            msgattr=luba_msg_pb2.MsgAttr.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            nav=mctrl_nav_pb2.MctlNav(
+                todev_taskctrl=mctrl_nav_pb2.NavTaskCtrl(
+                    type=1,
+                    action=1,
+                    result=0
+                )
+            )
+        )
+        return luba_msg.SerializeToString()
+
+    def pause_execute_task(self):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_NAV,
+            sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
+            rcver=luba_msg_pb2.MsgDevice.DEV_MAINCTL,
+            msgattr=luba_msg_pb2.MsgAttr.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            nav=mctrl_nav_pb2.MctlNav(
+                todev_taskctrl=mctrl_nav_pb2.NavTaskCtrl(
+                    type=1,
+                    action=2,
+                    result=0
+                )
+            )
+        )
+
+        return luba_msg.SerializeToString()
+
+    async def return_to_dock(self):
+        mctrlNav = mctrl_nav_pb2.MctlNav()
+        navTaskCtrl = mctrl_nav_pb2.NavTaskCtrl()
+        navTaskCtrl.type = 1
+        navTaskCtrl.action = 5
+        navTaskCtrl.result = 0
+        mctrlNav.todev_taskctrl.CopyFrom(navTaskCtrl)
+
+        lubaMsg = luba_msg_pb2.LubaMsg()
+        lubaMsg.msgtype = luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_NAV
+        lubaMsg.sender = luba_msg_pb2.MsgDevice.DEV_MOBILEAPP
+        lubaMsg.rcver = luba_msg_pb2.MsgDevice.DEV_MAINCTL
+        lubaMsg.msgattr = luba_msg_pb2.MsgAttr.MSG_ATTR_REQ
+        lubaMsg.seqs = 1
+        lubaMsg.version = 1
+        lubaMsg.subtype = 1
+        lubaMsg.nav.CopyFrom(mctrlNav)
+        return lubaMsg.SerializeToString()
 
     def get_report_cfg(self, timeout: int = 10000, period: int = 1000, no_change_period: int = 2000):
         mctlsys = mctrl_sys_pb2.MctlSys(
