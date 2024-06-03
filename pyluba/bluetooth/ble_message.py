ebd55d75a9c3a7e8ca01bc7268294a6fdea86b3f
jLynx <admin@jlynx.net>
2024-06-03 22:22:13 +1200

    Added missing for sendOrderMsg_Video

--- pyluba/bluetooth/ble_message.py
+++ pyluba/bluetooth/ble_message.py
@@ -20,6 +20,7 @@ from pyluba.data.model.execute_boarder import ExecuteBorder
 from pyluba.proto import (
     dev_net_pb2,
     luba_msg_pb2,
+    luba_mul_pb2,
     mctrl_driver_pb2,
     mctrl_nav_pb2,
     mctrl_ota_pb2,
@@ -1252,3 +1253,23 @@ class BleMessage:
         }
         self.post_custom_data(
             self.get_json_string(bleOrderCmd.close_clear_connect_current_wifi, data).encode())
+
+    # === sendOrderMsg_Video ===
+    
+    async def send_order_msg_video(self, mul):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_MUL,
+            sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
+            rcver=luba_msg_pb2.MsgDevice.SOC_MODULE_MULTIMEDIA,
+            msgattr=luba_msg_pb2.MsgAttr.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            mul=mul)
+
+        return luba_msg.SerializeToString()
+
+    def device_agora_join_channel_with_position(self, enter_state: int):
+        position = luba_mul_pb2.MUL_CAMERA_POSITION.ALL if DeviceType.is_yu_ka(self.get_device_name()) else luba_mul_pb2.MUL_CAMERA_POSITION.LEFT
+        mctl_sys = luba_mul_pb2.SocMul(set_video=luba_mul_pb2.MulSetVideo(position=position, vi_switch=enter_state))
+        self.send_order_msg_video(mctl_sys)
