26210ef44e0a3767f9a4fb2589bb90199d6a9761
jLynx <admin@jlynx.net>
2024-06-03 21:39:02 +1200

    Added missine for sendOrderMsg_Driver

--- pyluba/bluetooth/ble_message.py
+++ pyluba/bluetooth/ble_message.py
@@ -715,24 +715,6 @@ class BleMessage:
         bytes = lubaMsg.SerializeToString()
         await self.post_custom_data_bytes(bytes)
 
-    async def setbladeHeight(self, height: int):
-        mctrlDriver = mctrl_driver_pb2.MctrlDriver()
-        drvKnifeHeight = mctrl_driver_pb2.DrvKnifeHeight()
-        drvKnifeHeight.knifeHeight = height
-        mctrlDriver.todev_knife_height_set.CopyFrom(drvKnifeHeight)
-
-        lubaMsg = luba_msg_pb2.LubaMsg()
-        lubaMsg.msgtype = luba_msg_pb2.MSG_CMD_TYPE_EMBED_DRIVER
-        lubaMsg.sender = luba_msg_pb2.DEV_MOBILEAPP
-        lubaMsg.rcver = luba_msg_pb2.DEV_MAINCTL
-        lubaMsg.msgattr = luba_msg_pb2.MSG_ATTR_REQ
-        lubaMsg.seqs = 1
-        lubaMsg.version = 1
-        lubaMsg.subtype = 1
-        lubaMsg.driver.CopyFrom(mctrlDriver)
-        bytes = lubaMsg.SerializeToString()
-        await self.post_custom_data_bytes(bytes)
-
     async def setBladeControl(self, onOff: int):
         mctlsys = mctrl_sys_pb2.MctlSys()
         sysKnifeControl = mctrl_sys_pb2.SysKnifeControl()
@@ -753,7 +735,7 @@ class BleMessage:
 
     async def start_job(self, blade_height):
         """Call after calling generate_route_information I think"""
-        await self.setbladeHeight(blade_height)
+        await self.set_knife_height(blade_height)
         await self.start_work_job()
 
     async def transformSpeed(self, linear: float, percent: float):
@@ -1252,3 +1234,55 @@ class BleMessage:
         }
         self.post_custom_data(
             self.get_json_string(bleOrderCmd.close_clear_connect_current_wifi, data).encode())
+
+# === sendOrderMsg_Driver ===
+
+    async def send_order_msg_driver(self, build):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_EMBED_DRIVER,
+            sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
+            rcver=luba_msg_pb2.MsgDevice.DEV_MAINCTL,
+            msgattr=luba_msg_pb2.MsgAttr.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            net=build)
+
+        return luba_msg.SerializeToString()
+
+    def set_knife_height(self, height: int):
+        print(f"Send knife height height={height}")
+        build = mctrl_driver_pb2.MctlDriver(todev_knife_hight_set=mctrl_driver_pb2.DrvKnifeHeight(knife_height=height))
+        print(f"Send command--Knife motor height setting height={height}")
+        self.send_order_msg_driver(build)
+
+    def set_speed(self, speed: float):
+        print(f"{self.get_device_name()} set speed, {speed}")
+        build = mctrl_driver_pb2.MctlDriver(bidire_speed_read_set=mctrl_driver_pb2.DrvSrSpeed(speed=speed, rw=1))
+        print(f"Send command--Speed setting speed={speed}")
+        self.send_order_msg_driver(build)
+
+    def syn_nav_star_point_data(self, sat_system: int):
+        build = mctrl_driver_pb2.MctlDriver(rtk_sys_mask_query=mctrl_driver_pb2.rtk_sys_mask_query_t(sat_system=sat_system))
+        print(f"Send command--Navigation satellite frequency point synchronization={sat_system}")
+        self.send_order_msg_driver(build)
+
+    def set_nav_star_point(self, cmd_req: str):
+        build = mctrl_driver_pb2.MctlDriver(rtk_cfg_req=mctrl_driver_pb2.rtk_cfg_req_t(cmd_req=cmd_req, cmd_length=len(cmd_req) - 1))
+        print(f"Send command--Navigation satellite frequency point setting={cmd_req}")
+        print(f"Navigation satellite setting, Send command--Navigation satellite frequency point setting={cmd_req}")
+        self.send_order_msg_driver(build)
+
+    def get_speed(self):
+        build = mctrl_driver_pb2.MctlDriver(bidire_speed_read_set=mctrl_driver_pb2.DrvSrSpeed(rw=0))
+        print("Send command--Get speed value")
+        self.send_order_msg_driver(build)
+
+    def operate_on_device(self, main_ctrl: int, cut_knife_ctrl: int, cut_knife_height: int, max_run_speed: float):
+        build = mctrl_driver_pb2.MctlDriver(mow_ctrl_by_hand=mctrl_driver_pb2.DrvMowCtrlByHand(main_ctrl=main_ctrl, cut_knife_ctrl=cut_knife_ctrl, cut_knife_height=cut_knife_height, max_run_speed=max_run_speed))
+        print(f"Send command--Manual mowing command, main_ctrl:{main_ctrl}, cut_knife_ctrl:{cut_knife_ctrl}, cut_knife_height:{cut_knife_height}, max_run_speed:{max_run_speed}")
+        self.send_order_msg_driver(build)
+
+    def send_control(self, linear_speed: int, angular_speed: int):
+        print(f"Control command print, linearSpeed={linear_speed} // angularSpeed={angular_speed}")
+        self.send_order_msg_driver(mctrl_driver_pb2.MctlDriver(todev_devmotion_ctrl=mctrl_driver_pb2.DrvMotionCtrl(set_linear_speed=linear_speed, set_angular_speed=angular_speed)))
