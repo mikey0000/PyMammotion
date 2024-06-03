494dbac6612e43057bbf9b40b66a49c8a1b37dcc
jLynx <admin@jlynx.net>
2024-06-03 22:13:52 +1200

    Updated send

--- pyluba/bluetooth/ble_message.py
+++ pyluba/bluetooth/ble_message.py
@@ -1254,7 +1254,7 @@ class BleMessage:
             self.get_json_string(bleOrderCmd.close_clear_connect_current_wifi, data).encode())
 
     # === sendOrderMsg_Ota ===
-    def send_order_msg_ota(self, build):
+    def send_order_msg_ota(self, ota):
         luba_msg = luba_msg_pb2.LubaMsg(
             msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_EMBED_OTA,
             sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
@@ -1263,7 +1263,7 @@ class BleMessage:
             seqs=1,
             version=1,
             subtype=1,
-            net=build)
+            ota=ota)
         
         return luba_msg.SerializeToString()
     
@@ -1272,9 +1272,9 @@ class BleMessage:
     def get_device_ota_info(self, log_type: int):        
         todev_get_info_req = mctrl_ota_pb2.MctlOta.Builder().todev_get_info_req.CopyFrom(mctrl_ota_pb2.getInfoReq.Builder().type = mctrl_ota_pb2.IT_OTA)
         print("===Send command to get upgrade details===logType:" + str(log_type))
-        self.send_order_msg_ota(todev_get_info_req, 52, False)
+        self.send_order_msg_ota(todev_get_info_req)
 
     def get_device_info_new(self):
         todev_get_info_req = mctrl_ota_pb2.MctlOta.Builder().todev_get_info_req.CopyFrom(mctrl_ota_pb2.getInfoReq.Builder().type = mctrl_ota_pb2.IT_BASE)
         print("Send to get OTA upgrade information", "Get device information")
-        self.send_order_msg_ota(todev_get_info_req, 53, True)
+        self.send_order_msg_ota(todev_get_info_req)
