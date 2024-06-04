4c035575a456d9c56f8cebb34b824f07aff452d3
jLynx <admin@jlynx.net>
2024-06-04 12:18:30 +1200

    Added missing for sendOrderMsg_Video (#25)

--- pyluba/bluetooth/ble_message.py
+++ pyluba/bluetooth/ble_message.py
@@ -1401,3 +1401,23 @@ class BleMessage:
 
     def set_car_wiper(self, round: int):
         self.send_order_msg_media(luba_mul_pb2.SocMul(set_wiper=luba_mul_pb2.MulSetWiper(round=round)))
+
+    # === sendOrderMsg_Video ===
+    
+    async def send_order_msg_video(self, mul):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MsgCmdType.MSG_CMD_TYPE_MUL,
+            sender=luba_msg_pb2.MsgDevice.DEV_MOBILEAPP,
+            rcver=luba_msg_pb2.MsgDevice.SOC_MODULE_MULTIMEDIA,
+            msgattr=luba_msg_pb2.MsgAttr.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            mul=mul)
+
+        return luba_msg.SerializeToString()
+
+    def device_agora_join_channel_with_position(self, enter_state: int):
+        position = luba_mul_pb2.MUL_CAMERA_POSITION.ALL if DeviceType.is_yu_ka(self.get_device_name()) else luba_mul_pb2.MUL_CAMERA_POSITION.LEFT
+        mctl_sys = luba_mul_pb2.SocMul(set_video=luba_mul_pb2.MulSetVideo(position=position, vi_switch=enter_state))
+        self.send_order_msg_video(mctl_sys)
