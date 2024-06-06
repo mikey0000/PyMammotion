522a2223603d163a54e9e52be7a369afe5fb6f4c
Michael Arthur <michael.arthur@gameglass.gg>
2024-06-07 10:33:46 +1200

    figure out the quirks for remote control

--- tests/pyjoystick_example.py
+++ tests/pyjoystick_example.py
@@ -23,53 +23,57 @@ class JoystickControl:
     angular_speed = 0
     ignore_events = False
     blade_height = 25
+    worker = None
 
     def __init__(self, client: BleMessage):
         self._client = client
         self._curr_time = timer()
+        
+        repeater = pyjoystick.HatRepeater(
+            first_repeat_timeout=0.2, repeat_timeout=0.03, check_timeout=0.01
+        )
+
+        self.mngr = pyjoystick.ThreadEventManager(
+            event_loop=run_event_loop,
+            handle_key_event=self.key_received,
+            add_joystick=self.print_add,
+            remove_joystick=self.print_remove,
+            button_repeater=repeater,
+        )
+        
+        self.worker = PeriodicThread(0.2, self.run_movement, name='luba-process_movements')
+        self.worker.alive = self.mngr.alive  # stop when this event stops
+        self.worker.daemon = True
 
     def _movement_finished(self):
         self.ignore_events = False
 
     def run_movement(self):
-        if self.linear_speed != 0 or self.angular_speed != 0:
-            asyncio.run(
-                self._client.transformBothSpeeds(
-                    self.linear_speed,
-                    self.angular_speed,
-                    self.linear_percent,
-                    self.angular_percent,
-                )
+
+        # print(self.linear_speed,
+        #         self.angular_speed,
+        #         self.linear_percent,
+        #         self.angular_percent)
+        asyncio.run(
+            self._client.transformBothSpeeds(
+                self.linear_speed,
+                self.angular_speed,
+                self.linear_percent,
+                self.angular_percent,
             )
+        )
+            
+    def print_add(self, joy):
+        print("Added", joy)
+
+    def print_remove(self, joy):
+        print("Removed", joy)
+
+    def key_received(self, key):
+        self.handle_key_received(key)
 
     def run_controller(self):
-
-        def print_add(joy):
-            print("Added", joy)
-
-        def print_remove(joy):
-            print("Removed", joy)
-
-        def key_received(key):
-            self.handle_key_received(key)
-
-        # run_event_loop(print_add, print_remove, key_received)
-        repeater = pyjoystick.HatRepeater(
-            first_repeat_timeout=0.2, repeat_timeout=0.03, check_timeout=0.01
-        )
-
-        mngr = pyjoystick.ThreadEventManager(
-            event_loop=run_event_loop,
-            handle_key_event=key_received,
-            add_joystick=print_add,
-            remove_joystick=print_remove,
-            button_repeater=repeater,
-        )
-        mngr.start()
-
-        self.worker = PeriodicThread(0.2, self.run_movement, name='luba-process_movements')
-        self.worker.alive = mngr.alive  # stop when this event stops
-        self.worker.daemon = True
+        self.mngr.start()
         self.worker.start()
 
 
@@ -80,8 +84,10 @@ class JoystickControl:
         return percent - 15.0
 
     def handle_key_received(self, key):
+        # print(key, "-", key.keytype, "-", key.number, "-", key.value)
+        
         if key.keytype is Key.BUTTON and key.value == 1:
-            print(key, "-", key.keytype, "-", key.number, "-", key.value)
+            # print(key, "-", key.keytype, "-", key.number, "-", key.value)
             if key.number == 0:  # x
                 asyncio.run(self._client.return_to_dock())
             if key.number == 1:
@@ -102,15 +108,8 @@ class JoystickControl:
                     asyncio.run(self._client.setbladeHeight(self.blade_height))
 
         if key.keytype is Key.AXIS:
+            # print(key, "-", key.keytype, "-", key.number, "-", key.value)
             if key.value > 0.09 or key.value < -0.09:
-                print(key, "-", key.keytype, "-", key.number, "-", key.value)
-                # ignore events for 200ms
-                elapsed_time = timer()
-                if (elapsed_time - self._curr_time) < 0.2:
-                    return
-                else:
-                    self._curr_time = timer()
-
                 match key.number:
                     case 1:  # left (up down)
                         # take left right values and convert to linear movement
@@ -143,21 +142,30 @@ class JoystickControl:
                             self.angular_percent = self.get_percent(abs(key.value * 100))
 
             else:
+                print("else")
+                match key.number:
+                    case 1:  # left (up down)
+                        self.linear_speed = 0.0
+                        self.linear_percent = 0.0
+                
 
-                if self.linear_speed != 0 or self.angular_speed != 0:
-                    self.linear_speed = 0.0
-                    self.linear_percent = 0
-                    self.angular_speed = 0.0
-                    self.angular_percent = 0
+                    case 2:  # right  (left right)
+                        self.angular_speed = 0.0
+                        self.angular_percent = 0.0
+                        
+                    # self.linear_speed = 0.0
+                    # self.linear_percent = 0
+                    # self.angular_speed = 0.0
+                    # self.angular_percent = 0
 
-                    asyncio.run(
-                        self._client.transformBothSpeeds(
-                            self.linear_speed,
-                            self.angular_speed,
-                            self.linear_percent,
-                            self.angular_percent,
-                        )
-                    )
+                    # asyncio.run(
+                    #     self._client.transformBothSpeeds(
+                    #         self.linear_speed,
+                    #         self.angular_speed,
+                    #         self.linear_percent,
+                    #         self.angular_percent,
+                    #     )
+                    # )
 
 async def run():
     bleLubaConn = LubaBLE(bleNotificationEvt)
