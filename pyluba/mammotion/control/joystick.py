74dd03b83dd8acfb62cc4c8f31240da2af62a91b
Michael Arthur <michael.arthur@gameglass.gg>
2024-06-18 09:20:01 +1200

    final tweaks

--- pyluba/mammotion/control/joystick.py
+++ pyluba/mammotion/control/joystick.py
@@ -7,9 +7,12 @@ from pyjoystick.sdl2 import Key, run_event_loop
 from pyjoystick.utils import PeriodicThread
 
 from pyluba.bluetooth.ble_message import BleMessage
+from pyluba.event import BleNotificationEvent
 
+bleNotificationEvt = BleNotificationEvent()
+
+import nest_asyncio
 nest_asyncio.apply()
-
 
 class JoystickControl:
     """Joystick class for controlling Luba with a joystick"""
@@ -20,58 +23,73 @@ class JoystickControl:
     angular_speed = 0
     ignore_events = False
     blade_height = 25
+    worker = None
 
-    def __init__(self, client: BleMessage):
-        self._client = client
+    def __init__(self, ble_message: BleMessage):
+        self._client = ble_message
         self._curr_time = timer()
+        self.stopped = False
+        
+        repeater = pyjoystick.HatRepeater(
+            first_repeat_timeout=0.2, repeat_timeout=0.03, check_timeout=0.01
+        )
+
+        self.mngr = pyjoystick.ThreadEventManager(
+            event_loop=run_event_loop,
+            handle_key_event=self.key_received,
+            add_joystick=self.print_add,
+            remove_joystick=self.print_remove,
+            button_repeater=repeater,
+        )
+        
+        self.worker = PeriodicThread(0.2, self.run_movement, name='luba-process_movements')
+        self.worker.alive = self.mngr.alive  # stop when this event stops
+        self.worker.daemon = True
 
     def _movement_finished(self):
         self.ignore_events = False
 
     def run_movement(self):
-        if self.linear_speed != 0 or self.angular_speed != 0:
-            asyncio.run(
-                self._client.transformBothSpeeds(
-                    self.linear_speed,
-                    self.angular_speed,
-                    self.linear_percent,
-                    self.angular_percent,
-                )
+        if (self.linear_percent == 0.0 and self.angular_percent == 0.0):
+            if self.stopped:
+                return
+            self.stopped = True
+        self.stopped = False
+            
+        asyncio.run(
+            self._client.transformBothSpeeds(
+                self.linear_speed,
+                self.angular_speed,
+                self.linear_percent,
+                self.angular_percent,
             )
+        )
+            
+    def print_add(self, joy):
+        print("Added", joy)
+
+    def print_remove(self, joy):
+        print("Removed", joy)
+
+    def key_received(self, key):
+        self.handle_key_received(key)
 
     def run_controller(self):
-
-        def print_add(joy):
-            print("Added", joy)
-
-        def print_remove(joy):
-            print("Removed", joy)
-
-        def key_received(key):
-            self.handle_key_received(key)
-
-        # run_event_loop(print_add, print_remove, key_received)
-        repeater = pyjoystick.HatRepeater(
-            first_repeat_timeout=0.2, repeat_timeout=0.03, check_timeout=0.01
-        )
-
-        mngr = pyjoystick.ThreadEventManager(
-            event_loop=run_event_loop,
-            handle_key_event=key_received,
-            add_joystick=print_add,
-            remove_joystick=print_remove,
-            button_repeater=repeater,
-        )
-        mngr.start()
-
-        self.worker = PeriodicThread(0.2, self.run_movement, name='luba-process_movements')
-        self.worker.alive = mngr.alive  # stop when this event stops
-        self.worker.daemon = True
+        self.mngr.start()
         self.worker.start()
 
+
+    def get_percent(self, percent: float):
+        if (percent <= 15.0):
+            return 0.0
+
+        return percent - 15.0
+
     def handle_key_received(self, key):
+        # print(key, "-", key.keytype, "-", key.number, "-", key.value)
+        
         if key.keytype is Key.BUTTON and key.value == 1:
-            print(key, "-", key.keytype, "-", key.number, "-", key.value)
+            # print(key, "-", key.keytype, "-", key.number, "-", key.value)
             if key.number == 0:  # x
                 asyncio.run(self._client.return_to_dock())
             if key.number == 1:
@@ -92,15 +110,8 @@ class JoystickControl:
                     asyncio.run(self._client.setbladeHeight(self.blade_height))
 
         if key.keytype is Key.AXIS:
+            # print(key, "-", key.keytype, "-", key.number, "-", key.value)
             if key.value > 0.09 or key.value < -0.09:
-                print(key, "-", key.keytype, "-", key.number, "-", key.value)
-                # ignore events for 200ms
-                elapsed_time = timer()
-                if (elapsed_time - self._curr_time) < 0.2:
-                    return
-                else:
-                    self._curr_time = timer()
-
                 match key.number:
                     case 1:  # left (up down)
                         # take left right values and convert to linear movement
@@ -112,10 +123,10 @@ class JoystickControl:
                         print("case 1")
                         if key.value > 0:
                             self.linear_speed = 270.0
-                            self.linear_percent = abs(key.value * 100)
+                            self.linear_percent = self.get_percent(abs(key.value * 100))
                         else:
                             self.linear_speed = 90.0
-                            self.linear_percent = abs(key.value * 100)
+                            self.linear_percent = self.get_percent(abs(key.value * 100))
 
                     case 2:  # right  (left right)
                         # take left right values and convert to angular movement
@@ -124,25 +135,19 @@ class JoystickControl:
                         # angular_speed==-450
                         # angular_speed==450
                         if key.value > 0:
-                            self.angular_speed = 360.0
-                            self.angular_percent = abs(key.value * 100)
+                            self.angular_speed = 0.0
+                            self.angular_percent = self.get_percent(abs(key.value * 100))
                         else:
                             # angle=180.0
                             # linear_speed=0//angular_speed=-450
                             self.angular_speed = 180.0
-                            self.angular_percent = abs(key.value * 100)
+                            self.angular_percent = self.get_percent(abs(key.value * 100))
 
-            elif self.linear_speed != 0 or self.angular_speed != 0:
-                self.linear_speed = 0.0
-                self.linear_percent = 0
-                self.angular_speed = 0.0
-                self.angular_percent = 0
-
-                asyncio.run(
-                    self._client.transformBothSpeeds(
-                        self.linear_speed,
-                        self.angular_speed,
-                        self.linear_percent,
-                        self.angular_percent,
-                    )
-                )
+            else:
+                match key.number:
+                    case 1:  # left (up down)
+                        self.linear_speed = 0.0
+                        self.linear_percent = 0.0
+                    case 2:  # right  (left right)
+                        self.angular_speed = 0.0
+                        self.angular_percent = 0.0
