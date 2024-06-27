fad72ba120f5b63a40e268cbf2b4ecbce629af78
jLynx <admin@jlynx.net>
2024-06-27 21:46:24 +1200

    WIP

--- pyluba/mammotion/commands/messages/navigation_old.py
+++ pyluba/mammotion/commands/messages/navigation_old.py
@@ -452,7 +452,7 @@ class MessageNavigation:
         lubaMsg.nav.CopyFrom(mctrlNav)
         return lubaMsg.SerializeToString()
 
-# =============== ToDo: What is this replaced with? ===============
+    # Replaced \w: auto_under_pile(self):
     def leave_dock(self):
         mctrlNav = mctrl_nav_pb2.MctlNav()
         mctrlNav.todev_one_touch_leave_pile = 1
