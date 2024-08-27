de5cd0ac0d2fbf69c819a6daac92cd637dd2ad29
Michael Arthur <michael.arthur@gameglass.gg>
2024-08-27 23:07:49 +1200

    fix issue with model having optional nickName

--- pymammotion/aliyun/dataclass/dev_by_account_response.py
+++ pymammotion/aliyun/dataclass/dev_by_account_response.py
@@ -9,7 +9,6 @@ from mashumaro.mixins.orjson import DataClassORJSONMixin
 class Device(DataClassORJSONMixin):
     gmtModified: int
     netType: str
-    nickName: str
     categoryKey: str
     productKey: str
     nodeType: str
@@ -24,9 +23,12 @@ class Device(DataClassORJSONMixin):
     identityId: str
     thingType: str
     status: int
+    nickName: Optional[str] = None
+    description: Optional[str] = None
     productImage: Optional[str] = None
     categoryImage: Optional[str] = None
     productModel: Optional[str] = None
+
 
     class Config(BaseConfig):
         omit_default = True
