b04ceebfeb628209d1d8ab988d99e21659c8a6a4
Michael Arthur <michael.arthur@gameglass.gg>
2025-02-19 16:47:46 +1300

    remove cloud service

--- pymammotion/aliyun/cloud_service.py
+++ pymammotion/aliyun/cloud_service.py
@@ -6,15 +6,20 @@ from aliyunsdkiot.request.v20180120.InvokeThingServiceRequest import (
     InvokeThingServiceRequest,
 )
 
+##
+#aliyun-python-sdk-core = "^2.16.0"
+#aliyun-python-sdk-iot = "^8.59.0"
+##
 
-class CloudService:
-    # com.aliyun.iot.aep.sdk
-    # https://domestic.mammotion.com/privacy/ - lists all aliyun packages
-    def __init__(self) -> None:
-        self.selectDeviceIOTID = ""
-        accessKeyId = "<your accessKey>"
-        accessKeySecret = "<your accessSecret>"
-        self.clt = client.AcsClient(accessKeyId, accessKeySecret, "ap-southeast")
+
+# class CloudService:
+#     # com.aliyun.iot.aep.sdk
+#     # https://domestic.mammotion.com/privacy/ - lists all aliyun packages
+#     def __init__(self) -> None:
+#         self.selectDeviceIOTID = ""
+#         accessKeyId = "<your accessKey>"
+#         accessKeySecret = "<your accessSecret>"
+#         self.clt = client.AcsClient(accessKeyId, accessKeySecret, "ap-southeast")
 
     """
     String printBase64Binary = DatatypeConverter.printBase64Binary(byteArray);
@@ -30,36 +35,36 @@ class CloudService:
         }
     
     """
-
-    def invoke_thing_service(self, data: bytearray) -> None:
-        base64_encoded = base64.b64encode(data).decode("utf-8")
-
-        # Create a dictionary structure
-        data = {
-            "args": {"content": base64_encoded},
-            "DEVICE_IOTID": self.selectDeviceIOTID,
-            "identifier": "device_protobuf_sync_service",
-        }
-
-        request = InvokeThingServiceRequest()
-        request.set_accept_format("json")
-
-        request.set_Args("Args")
-        request.set_Identifier("Identifier")
-        request.set_IotId("IotId")
-        request.set_ProductKey("ProductKey")
-
-        response = self.clt.do_action_with_exception(request)
-        # python2:  print(response)
-        print(response)
-
-    def get_device_status(self) -> None:
-        request = GetDeviceStatusRequest()
-        request.set_accept_format("json")
-
-        request.set_IotId("IotId")
-        request.set_ProductKey("ProductKey")
-        request.set_DeviceName("DeviceName")
-
-        response = self.clt.do_action_with_exception(request)
-        print(response)
+    #
+    # def invoke_thing_service(self, data: bytearray) -> None:
+    #     base64_encoded = base64.b64encode(data).decode("utf-8")
+    #
+    #     # Create a dictionary structure
+    #     data = {
+    #         "args": {"content": base64_encoded},
+    #         "DEVICE_IOTID": self.selectDeviceIOTID,
+    #         "identifier": "device_protobuf_sync_service",
+    #     }
+    #
+    #     request = InvokeThingServiceRequest()
+    #     request.set_accept_format("json")
+    #
+    #     request.set_Args("Args")
+    #     request.set_Identifier("Identifier")
+    #     request.set_IotId("IotId")
+    #     request.set_ProductKey("ProductKey")
+    #
+    #     response = self.clt.do_action_with_exception(request)
+    #     # python2:  print(response)
+    #     print(response)
+    #
+    # def get_device_status(self) -> None:
+    #     request = GetDeviceStatusRequest()
+    #     request.set_accept_format("json")
+    #
+    #     request.set_IotId("IotId")
+    #     request.set_ProductKey("ProductKey")
+    #     request.set_DeviceName("DeviceName")
+    #
+    #     response = self.clt.do_action_with_exception(request)
+    #     print(response)
