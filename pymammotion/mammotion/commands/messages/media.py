7099b75a493eda7506b8786b8379d074f6f946dd
Michael Arthur <michael.arthur@gameglass.gg>
2025-05-08 09:37:59 +1200

    fix mul language issue

--- pymammotion/mammotion/commands/messages/media.py
+++ pymammotion/mammotion/commands/messages/media.py
@@ -2,7 +2,8 @@
 from abc import ABC
 
 from pymammotion.mammotion.commands.abstract_message import AbstractMessage
-from pymammotion.proto import LubaMsg, MsgAttr, MsgCmdType, MsgDevice, MulLanguage, MulSetAudio, MulSetWiper, SocMul
+from pymammotion.proto import (LubaMsg, MsgAttr, MsgCmdType, MsgDevice, MulSetAudio, MulSetWiper, SocMul,
+    MUL_SEX, MUL_LANGUAGE)
 
 
 class MessageMedia(AbstractMessage, ABC):
@@ -24,10 +25,15 @@ class MessageMedia(AbstractMessage, ABC):
         """Set the car volume. 0 - 100"""
         return self.send_order_msg_media(SocMul(set_audio=MulSetAudio(at_switch=volume)))
 
-    def set_car_voice_language(self, language_type: MulLanguage | str | None):
+    def set_car_voice_language(self, language_type: MUL_LANGUAGE | str | None):
         return self.send_order_msg_media(SocMul(set_audio=MulSetAudio(au_language=language_type)))
+
+    def set_car_volume_sex(self, sex: MUL_SEX):
+        return self.send_order_msg_media(SocMul(set_audio=MulSetAudio(sex=sex)))
 
     def set_car_wiper(self, round_num: int):
         """Set mower wiper."""
         # 2
         return self.send_order_msg_media(SocMul(set_wiper=MulSetWiper(round=round_num)))
+
+    # TODO add lamp methods
