3086cf219ddac413eb0c346f87bc24c12001f75b
Michael Arthur <michael.arthur@gameglass.gg>
2025-07-31 21:49:28 +1200

    further updates to support multiple subscribers to events

--- pymammotion/event/event.py
+++ pymammotion/event/event.py
@@ -1,21 +1,42 @@
 import asyncio
+from collections.abc import Callable
+from types import MethodType
 from typing import Any
+import weakref
 
 
 class Event:
     def __init__(self) -> None:
-        self.__eventhandlers = []
+        self.__eventhandlers: list[weakref.ReferenceType] = []
 
-    def __iadd__(self, handler):
-        self.__eventhandlers.append(handler)
+    def __iadd__(self, handler: Callable) -> "Event":
+        if isinstance(handler, MethodType):
+            # Instance method, use WeakMethod
+            ref = weakref.WeakMethod(handler)
+        else:
+            # Function or static method, use weakref.ref
+            ref = weakref.ref(handler)
+        self.__eventhandlers.append(ref)
         return self
 
-    def __isub__(self, handler):
-        self.__eventhandlers.remove(handler)
+    def __isub__(self, handler: Callable) -> "Event":
+        self.__eventhandlers = [ref for ref in self.__eventhandlers if ref() is not handler]
         return self
 
     async def __call__(self, *args: Any, **kwargs: Any) -> None:
-        await asyncio.gather(*[handler(*args, **kwargs) for handler in self.__eventhandlers])
+        live_handlers = []
+        for ref in self.__eventhandlers:
+            func = ref()
+            if func is not None:
+                live_handlers.append(func(*args, **kwargs))
+        await asyncio.gather(*live_handlers)
+
+        # Clean up dead references
+        self.__eventhandlers = [ref for ref in self.__eventhandlers if ref() is not None]
+
+    def has_dead_handlers(self) -> bool:
+        """Check if any handlers have been garbage collected."""
+        return any(ref() is None for ref in self.__eventhandlers)
 
 
 class MoveEvent:
