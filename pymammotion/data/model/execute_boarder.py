4500410fe61dd9345551dd446ed95abaa93d748a
Michael Arthur <michael.arthur@gameglass.gg>
2025-11-18 12:08:05 +1300

    fix futher issues with execute_border

--- pymammotion/data/model/execute_boarder.py
+++ pymammotion/data/model/execute_boarder.py
@@ -7,15 +7,15 @@ class ExecuteBorder(Serializable):
     """generated source for class ExecuteBorderBean"""
 
     cmd = 0
-    params = ExecuteBorderParams(None, None, None)
+    params = ExecuteBorderParams()
 
-    def __init__(self, i, execute_border_params: ExecuteBorderParams) -> None:
+    def __init__(self, i: int, execute_border_params: ExecuteBorderParams) -> None:
         """Generated source for method __init__"""
         super().__init__()
         self.cmd = i
         self.params = execute_border_params
 
-    def get_cmd(self):
+    def get_cmd(self) -> int:
         """Generated source for method getCmd"""
         return self.cmd
 
@@ -23,14 +23,14 @@ class ExecuteBorder(Serializable):
         """Generated source for method setCmd"""
         self.cmd = i
 
-    def get_params(self):
+    def get_params(self) -> ExecuteBorderParams:
         """Generated source for method getParams"""
         return self.params
 
-    def set_params(self, execute_border_params) -> None:
+    def set_params(self, execute_border_params: ExecuteBorderParams) -> None:
         """Generated source for method setParams"""
         self.params = execute_border_params
 
     def __str__(self) -> str:
         """Generated source for method toString"""
-        return "ExecuteBean{cmd=" + self.cmd + ", params=" + self.params + "}"
+        return f"ExecuteBean cmd={self.cmd}, params={self.params}"
