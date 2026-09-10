5b3f5254e3af09d41d0f4acc13743a60334063a6
Michael Arthur <michael.arthur@gameglass.gg>
2026-09-10 18:58:13 +1200

    release: 0.9.0b4

--- pymammotion/__init__.py
+++ pymammotion/__init__.py
@@ -21,7 +21,7 @@ with contextlib.suppress(ImportError):
 from pymammotion.bluetooth.ble import MammotionBLE
 from pymammotion.http.http import MammotionHTTP
 
-__version__ = "0.9.0b3"
+__version__ = "0.9.0b4"
 
 logger = logging.getLogger(__name__)
 
