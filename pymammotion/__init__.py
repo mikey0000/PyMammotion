49a3c8c5f5700d0580efd235dd71b650f1f8e785
Michael Arthur <michael.arthur@gameglass.gg>
2026-09-12 21:23:19 +1200

    Bump version 0.9.0b4 -> 0.9.0b5

--- pymammotion/__init__.py
+++ pymammotion/__init__.py
@@ -21,7 +21,7 @@ with contextlib.suppress(ImportError):
 from pymammotion.bluetooth.ble import MammotionBLE
 from pymammotion.http.http import MammotionHTTP
 
-__version__ = "0.9.0b4"
+__version__ = "0.9.0b5"
 
 logger = logging.getLogger(__name__)
 
